9e96fbd7136da7d678a858f162ce7dcf38928515
Tran Tien Dat <trantiendat.dev@gmail.com>
2024-01-10 23:46:22 +0700

    update package manager and init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,5 +4,6 @@ require('core.colorscheme')
 require('plugins.plugins')
 require('plugins.alpha')
 require('plugins.lualine')
+require('plugins.bufferline')
 require('custom.discord')
 
--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -2,9 +2,15 @@ return require('packer').startup(function()
 	-- Packer manage itself
 	use 'wbthomason/packer.nvim'
 	-- Colorscheme
-	use 'EdenEast/nightfox.nvim'
+	use 'folke/tokyonight.nvim'
+	-- Icons
+	use 'nvim-tree/nvim-web-devicons'
 	-- Dashboard
 	use 'goolord/alpha-nvim'
 	-- Status bar
 	use 'nvim-lualine/lualine.nvim'
+	-- Tab bar
+	use 'akinsho/bufferline.nvim'
+	-- Discord Rich Presence
+	use 'andweeb/presence.nvim'
 end)
