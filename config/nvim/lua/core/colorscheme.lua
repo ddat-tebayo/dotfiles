cbfcda769e1589491bfa6e0fd1ba3ac6e01c3b4d
Tran Tien Dat <trantiendat.dev@gmail.com>
2024-01-28 13:28:23 +0700

    Migrate neovim configuration to separate repository

--- config/nvim/lua/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-require('nightfox').setup({
-  options = {
-    transparent = true
-  }
-})
-
--- setup must be called before loading
-vim.cmd("colorscheme nightfox")
--- config/nvim/lua/custom/neovide.lua
+++
@@ -1,4 +0,0 @@
-if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono_Nerd_Font:h9.5"
-  vim.g.neovide_hide_mouse_when_typing = true
-end
